672054c0848920aa0aff3820545908317db12784
fasbhim <https://gitlab.argonet.co.kr/Fas96>
2022-10-30 17:47:36 +0900

    "map popUpInfo"

--- frontend/src/map/customMap.ts
+++ frontend/src/map/customMap.ts
@@ -1,11 +1,35 @@
+import {displayLocation} from "@/util/commonUtils";
+
+
 export  class Mappable {
     location :  {
      lat :  number ;
      lng :  number ;
       };
-    markerContent ():  string  {
-      return   '' ;
-      }
+      markerContent(): Element {
+        let mapContent = document.createElement('div');
+        mapContent.classList.add('map-content');
+          displayLocation(this.location.lat, this.location.lng).then((placeName) => {
+              let mapContentName3 = document.createElement('div');
+              mapContentName3.classList.add('map-content-name');
+              mapContentName3.innerHTML ='Name:'+ placeName;
+              mapContent.appendChild(mapContentName3);
+          }) ;
+
+        let mapContentName = document.createElement('div');
+        mapContentName.classList.add('map-content-name');
+        mapContentName.innerHTML = `Lat: ${this.location.lat.toString()}`;
+
+        let mapContentName2 = document.createElement('div');
+        mapContentName2.classList.add('map-content-name');
+        mapContentName2.innerHTML = `Lng: ${this.location.lng.toString()}`;
+        mapContent.appendChild(mapContentName);
+
+        mapContent.appendChild(mapContentName2);
+
+
+        return mapContent;
+    }
     color :  string  =  'red' ;
 }
 
@@ -33,7 +57,12 @@ export class CustomMap{
             const infoWindow = new google.maps.InfoWindow({
                 content: mappable.markerContent()
             })
+
             infoWindow.open(this.googleMap, marker)
+
+            this.googleMap.setZoom(2);
+            this.googleMap.setCenter(marker.getPosition() as google.maps.LatLng);
+
         })
     }
 
--- frontend/src/util/commonUtils.ts
+++ frontend/src/util/commonUtils.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const   getContext=()=> {
     let context = window.location.pathname.substring(0, window.location.pathname.indexOf("/", 2));
     if (context === "/") {
@@ -14,3 +16,21 @@ export const storeDataInSession=(data: FCDSUser)=>{
     sessionStorage.setItem('failCount', data.failCount);
     sessionStorage.setItem('isBlocked', String(data.isBlocked));
 }
+
+
+export  const  displayLocation=async (latitude, longitude): Promise<string> => {
+
+    let url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=AIzaSyAF3dVDB9988nqO0Qo5RCqbowh03x0GBs4';
+    let placeName = "Unknown";
+    await axios.get(url).then((response) => {
+        if (response.data.status === "OK") {
+            let data = response.data;
+            debugger;
+            placeName= data['plus_code']['compound_code'];
+        }
+
+    }).catch((error) => {
+        console.log(error);
+    })
+    return placeName;
+};
