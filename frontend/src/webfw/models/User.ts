ab44f36751e78ef6d82bfe1a9a8e433571b4a2f4
fasbhim <https://gitlab.argonet.co.kr/Fas96>
2022-11-27 20:56:54 +0900

    json-server"

--- frontend/src/webfw/models/User.ts
+++ frontend/src/webfw/models/User.ts
@@ -5,10 +5,12 @@ interface UserProps {
     name: string;
     age: number;
 }
-type Callback = () => {  };
+
+type Callback = () => void;
 
 export class User{
     private events:  { [key: string]: Callback[] } = {};
+
     constructor(private data:UserProps) {}
     get (propName: string): (number | string) {
         return this.data[propName];
